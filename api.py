cac9c294a1e768d8004ac59fb6cadc9ad4e060aa
Hackbright Student <no-reply@hackbrightacademy.com>
2019-11-04 13:58:19 -0800

    Database update

--- api.py
+++ api.py
@@ -13,8 +13,78 @@ INPUT_DATE_FORMAT = "%Y-%m-%d"
 # generate start date and end date
 # send request
 # get park information
-# get name of campsite
+# get campsite id
 # get num of available sites
 # validate dates entered are in the future
 # validate phone number entered
 # check avialability (main)
+
+
+def get_campsite_id(campsite_name):
+    campsite_name = resp['campground']['facility_name']
+    # if the required information is in the request, look for campsite
+
+    # FIX ME: look up campsite id with name
+
+    if selected_campsite:
+        payload - {}
+        headers = {}
+        response = requests.get()
+        data = response.json()
+
+        # How can I package all of this info at the end when phone is submitted?
+
+        # store selected site in database
+        campsite = Request(selected_campsite=name)
+
+        # Use selected campsite to look up campsite ID and store in DB
+        db.session.add(campsite)
+        db.session.commit()
+        
+    return campsite_id
+
+
+def format_date(date_obj):
+    # FIX ME
+
+    #return formatted_date
+
+
+def valid_dates():
+    # FIX ME
+    #if current_date > date_start OR if invalid format:
+        # flash("Select a valid date ")
+
+
+def generate_params(start, end):
+    # FIX ME
+
+    #return params
+
+
+def send_request(url, params):
+    # payload = {}
+    # request = requests.get(url, params=payload)
+
+    # return response.json
+
+
+def get_campsite_information(campsite_id, params):
+    url = "{}{}{}".format(BASE_URL, AVAILABILITY_ENDPOINT, site_id)
+
+    return send_request(url, params)
+
+
+def num_of_available_sites(resp, start_date, end_date):
+    # FIX ME
+
+    #return num_available, total_sites
+
+
+def main(campsite_id // campsite_name):
+    # FIX ME -- which arg to pass?
+
+    #return what is available or sold out
+
+
+def seed_db():
--- server.py
+++ server.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template, request, flash, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
 
 from model import connect_to_db, db, User, Campsite, Request
-import api
+# import api
 
 
 app = Flask(__name__)
@@ -18,90 +18,82 @@ app = Flask(__name__)
 app.jinja_env.undefined = StrictUndefined
 
 
-@app.route('/')
-def index():
-    """Homepage."""
+# @app.route('/')
+# def index():
+#     """Homepage."""
 
-    return render_template("homepage.html")
-
-
-@app.route('/', methods=['GET'])
-def campsite_search():
-    """Show homepage and form for campsite search"""
-
-    return render_template("campsite_search.html")
+#     return render_template("homepage.html")
 
 
-@app.route('/', methods=['POST'])
-def process_search():
-    """Process campsite search and selection"""
+# @app.route('/', methods=['GET'])
+# def campsite_search():
+#     """Show homepage and form for campsite search"""
 
-    # Get form variables
-    selected_campsite = request.form["selected_campsite"]
-
-    payload - {}
-    headers = {}
-    response = requests.get()
-    data = response.json()
-
-    # How can I package all of this info at the end when phone is submitted?
-    campsite = Request(selected_campsite=name)
-    # Use selected campsite to look up campsite ID and store in DB
-
-    db.session.add(campsite)
-    db.session.commit()
-    
-    return redirect(f"/dates")
+#     return render_template("campsite_search.html")
 
 
-@app.route('/dates', methods=['GET'])
-# should I store campsite in URL?
-def date_selector():
-    """Show calendar to select check in, check out dates"""
+# @app.route('/', methods=['POST'])
+# def process_search():
+#     """Process campsite search and selection"""
 
-    return render_template("calendar.html")
+#     # Get form variables
+#     campsite_name = request.form["selected_campsite"]
 
+#     get_campsite_id(campsite_name)
 
-@app.route('/dates', methods=['POST'])
-def process_dates(start, end):
-    """Process dates selected"""
-
-    # Get form variables
-   	date_start = request.form["date_start"]
-    date_end = request.form["date_end"]
-
-    # current_date = get today's datetime
-
-    #if current_date > date_start:
-        flash("Select a date in the future")
-
-    return redirect(f"/submit")
+#     return redirect(f"/dates")
 
 
-@app.route('/submit', methods=['GET'])
-# should I store previous selections in URL?
-def submission_form():
-    """Collect phone number and display previous selections"""
+# @app.route('/dates', methods=['GET'])
+# # should I store campsite in URL?
+# def date_selector():
+#     """Show calendar to select check in, check out dates"""
 
-    return render_template("submission_form.html")
-
-
-@app.route('/submit', methods=['POST'])
-def process_request():
-    """Process request for campsite notification"""
-
-    # Get form variables
-   	phone = request.form["phone"]
-
-    #created_at = get today's datetime
-
-    return redirect(f"/confirmation")
+#     return render_template("calendar.html")
 
 
-@app.route('/confirmation', methods=['GET'])
-# should I store previous selections in URL?
-def confirmation():
-    """Confirm submitted information"""
+# @app.route('/dates', methods=['POST'])
+# def process_dates(start, end):
+#     """Process dates selected"""
 
-    return render_template("confirmation_page.html")
+#     # Get form variables
+#    	date_start = request.form["date_start"]
+#     date_end = request.form["date_end"]
+
+#     # date_obj = 
+
+#     # current_date = get today's datetime
+#     valid_date(date_obj)
+
+#     return redirect(f"/submit")
+
+
+# @app.route('/submit', methods=['GET'])
+# # should I store previous selections in URL?
+# def submission_form():
+#     """Collect phone number and display previous selections"""
+
+#     #additionally display current availability
+
+#     return render_template("submission_form.html")
+
+
+# @app.route('/submit', methods=['POST'])
+# def process_request():
+#     """Process request for campsite notification"""
+
+#     # Get form variables
+#    	phone = request.form["phone"]
+
+#     #created_at = get today's datetime
+
+#     return redirect(f"/confirmation")
+
+
+# @app.route('/confirmation', methods=['GET'])
+# # should I store previous selections in URL?
+# def confirmation():
+#     """Confirm submitted information"""
+
+#     return render_template("confirmation_page.html")
 
