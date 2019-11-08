24e386474cd5b2ff5f0726807922bdb28f9fde86
Brittany Morton <bnmorton264@gmail.com>
2019-11-08 15:24:52 -0800

    HTML updates

---
+++ templates/datepicker.js
@@ -0,0 +1,11 @@
+var date = new Date();
+
+var day = date.getDate();
+var month = date.getMonth() + 1;
+var year = date.getFullYear();
+
+if (month < 10) month = "0" + month;
+if (day < 10) day = "0" + day;
+
+var today = year + "-" + month + "-" + day;       
+document.getElementById("theDate").value = today;
