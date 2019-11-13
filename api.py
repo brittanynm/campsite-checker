386eafb5b70fdd641cd26aea8caa829979ff3dda
Brittany Morton <bnmorton264@gmail.com>
2019-11-12 21:30:11 -0800

    availability check progress

--- api.py
+++ api.py
@@ -1,80 +1,53 @@
 import requests
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
-import server
-
-#search URL
-# base_search_url = "https://ridb.recreation.gov/api/vi/campsites"
+# import server
 
 # API availability information
 BASE_URL = "http://www.recreation.gov"
 AVAILABILITY_ENDPOINT = "/api/camps/availability/campground/"
-MAIN_PAGE_ENDPOINT = "/api/camps/campgrounds/"
-
-INPUT_DATE_FORMAT = "%Y-%m-%d"
 
 user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
 
 headers = {'User-Agent': user_agent}
 
 
-def format_date(date_object):
-    date_formatted = datetime.strftime(date_object, "%Y-%m-%dT00:00:00Z")
-    return date_formatted
+def availability(date_start, date_end, site_id):
+    q = "?start_date=" + date_start + "T00%3A00%3A00.000Z&end_date=" + date_end + "T00%3A00%3A00.000Z"
+    url = BASE_URL + AVAILABILITY_ENDPOINT + site_id + q
 
-
-def generate_params(start, end):
-    params = {"start_date": format_date(start), "end_date": format_date(end)}
-    return params
-
-
-def send_request(url, params):
-    resp = requests.get(url, params=params, headers=headers)
-    if resp.status_code != 200:
-        raise RuntimeError(
-            "failedRequest",
-            "ERROR, {} code received from {}: {}".format(
-                resp.status_code, url, resp.text
-            ),
-        )
+    resp = requests.get(url, headers=headers)
+    
     return resp.json()
-
-def get_park_information(park_id, params):
-    url = "{}{}{}".format(BASE_URL, AVAILABILITY_ENDPOINT, park_id)
-    return send_request(url, params)
 
 
 def get_num_available_sites(resp, start_date, end_date):
     maximum = resp["count"]
-
     num_available = 0
-    num_days = (end_date - start_date).days
-    dates = [end_date - timedelta(days=i) for i in range(1, num_days + 1)]
-    dates = set(format_date(i) for i in dates)
+    
     for site in resp["campsites"].values():
         available = bool(len(site["availabilities"]))
-        for date, status in site["availabilities"].items():
-            if date not in dates:
-                continue
+        for key, status in site["availabilities"].items():
             if status != "Available":
                 available = False
                 break
         if available:
             num_available += 1
-            return True
-    return num_available, maximum
+    print(num_available, "site(s) available out of", maximum, "site(s)")
 
 
-def send_avail_request(url, params):
-    payload = {}
-    request = requests.get(url, params=payload)
-
-    return response.json
+resp = availability('2019-12-01', '2019-12-04', '234513')
+get_num_available_sites(resp, '2019-12-01', '2019-12-04')
 
 
-# def main(campsite_id):
+# def send_avail_request(url, params):
+#     payload = {}
+#     request = requests.get(url, params=payload)
 
-    #return what is available or sold out
+#     return response.json
+
+
+
 
 
 # def seed_db():
