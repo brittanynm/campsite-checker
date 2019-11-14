a9d7fd5b1431e8eaec65b62fe110f6db1d7424a0
Brittany Morton <bnmorton264@gmail.com>
2019-11-13 17:27:41 -0800

    WIP

--- api.py
+++ api.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
 # import server
 
-# API availability information
 BASE_URL = "http://www.recreation.gov"
 AVAILABILITY_ENDPOINT = "/api/camps/availability/campground/"
 
@@ -12,9 +11,9 @@ user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 
 headers = {'User-Agent': user_agent}
 
 
-def availability(date_start, date_end, site_id):
-    q = "?start_date=" + date_start + "T00%3A00%3A00.000Z&end_date=" + date_end + "T00%3A00%3A00.000Z"
-    url = BASE_URL + AVAILABILITY_ENDPOINT + site_id + q
+def check_availability(date_start, date_end, site_id):
+    query = "?start_date=" + date_start + "T00%3A00%3A00.000Z&end_date=" + date_end + "T00%3A00%3A00.000Z"
+    url = BASE_URL + AVAILABILITY_ENDPOINT + site_id + query
 
     resp = requests.get(url, headers=headers)
     
@@ -37,8 +36,7 @@ def get_num_available_sites(resp, start_date, end_date):
     return f'{num_available} sites available out of {maximum} sites'
     
 
-
-resp = availability('2019-12-01', '2019-12-04', '234513')
+resp = check_availability('2019-12-01', '2019-12-04', '234513')
 get_num_available_sites(resp, '2019-12-01', '2019-12-04')
 
 
@@ -49,7 +47,3 @@ get_num_available_sites(resp, '2019-12-01', '2019-12-04')
 #     return response.json
 
 
-
-
-
-# def seed_db():
--- server.py
+++ server.py
@@ -11,12 +11,11 @@ from flask_wtf import Form
 from wtforms import DateField
 from datetime import date
 from lookup import is_valid_number
-from api import availability, get_num_available_sites
+from api import check_availability, get_num_available_sites
 
 app = Flask(__name__)
 
 app.secret_key = 'ABC'
-# Raises an error if you use an undefined variable in Jinja2
 app.jinja_env.undefined = StrictUndefined
 
 
@@ -32,12 +31,9 @@ def index():
 def search():
     """Process campsite search"""
     if request.method == 'GET':
-        # Get form variable
         campsite_name = request.args.get("query")
-        # Search database by name and park
         q = Campsite.query
         campsites = q.filter( (Campsite.name.ilike(f'%{campsite_name}%')) | (Campsite.park.ilike(f'%{campsite_name}%'))).all()
-        campsites = campsites
         return render_template("homepage.html", campsites=campsites)
     else:
         selected_site = request.form["selected_site"]
@@ -48,6 +44,7 @@ def search():
         session["site_name"] = site_name
         park_name = site_obj.park
         session["park_name"] = park_name
+    
     return redirect("/dates")
 
 
@@ -70,37 +67,32 @@ def process_dates():
 
 
 @app.route('/submit', methods=['GET', 'POST'])
-# should I store previous selections in URL?
 def submission_form():
-    """Collect phone number and display previous selections"""
+    """Collect phone number and display previous selections with availability"""
     if request.method == 'GET':
-        # display current availability
-        resp = availability(session["date_start"], session["date_end"], session["site_id"])
+        resp = check_availability(session["date_start"], session["date_end"], session["site_id"])
         available = get_num_available_sites(resp, session["date_start"], session["date_end"])
         # if available == False:
         #     available = "0 sites available"
         # else:
         #     available = "Sites are currently available. Go to recreation.gov to book!"
-
         return render_template("submission_form.html", 
                             site_name=session['site_name'],
                             date_start=session["date_start"], 
                             date_end=session["date_end"],
                             available=available)
     else:
-        # Get form variables
         phone = request.form["phone"]
         if is_valid_number(phone) == True:
             new_user = User(phone=phone)
             db.session.add(new_user)
             db.session.commit()
             user_id = new_user.user_id
-            
             session["user_id"] = user_id
-
             new_request = Request(user_id=session["user_id"], campsite_id=session["site_id"], date_start=session["date_start"], date_end=session["date_end"])
             db.session.add(new_request)
             db.session.commit()
+            
             return redirect("/")
         else:
             print("Invalid number")
@@ -114,6 +106,5 @@ if __name__ == "__main__":
     # Change to False for demo
     app.debug = True
     connect_to_db(app)
-    # Use the DebugToolbar
     DebugToolbarExtension(app)
     app.run(host="0.0.0.0")
