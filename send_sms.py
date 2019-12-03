1cded5b40570f9370e4f736aeaa94a0ea5b7ffdc
Brittany Morton <bnmorton264@gmail.com>
2019-12-03 15:37:15 -0800

    removed specifics

--- send_sms.py
+++ send_sms.py
@@ -3,9 +3,7 @@ from twilio.rest import Client
 
 
 # Your Account Sid and Auth Token from twilio.com/console
-# DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'AC8c5ba618686ac695cb965f2d3830354e'
-auth_token = 'cc3dc5e4307e313577af1029959f7a87'
+
 client = Client(account_sid, auth_token)
 
 def send_text(phone, msg):
