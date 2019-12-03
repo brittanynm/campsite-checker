5ddf9f3afa6921d3a9b2750f6eacfd60fcfb6690
Brittany Morton <bnmorton264@gmail.com>
2019-12-03 15:42:09 -0800

    formatting

--- send_sms.py
+++ send_sms.py
@@ -1,9 +1,6 @@
 # Download the helper library from https://www.twilio.com/docs/python/install
 from twilio.rest import Client
 
-
-# Your Account Sid and Auth Token from twilio.com/console
-
 client = Client(account_sid, auth_token)
 
 def send_text(phone, msg):
